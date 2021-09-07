2b3fc86e507165108f6cda1397d88bcf620b0cb7
jyjeong-placea <jy.jeong@placea.co.kr>
2021-09-07 20:46:40 +0900

    [feat] update work page about desktop

---
+++ html/js/works_scrolls.js
@@ -0,0 +1,58 @@
+var init_scroll_move = false;
+var sub_header = document.getElementById('sub_header');
+var sub_header_height = sub_header.clientHeight;
+var header_contents = document.getElementById('header_contents');
+var header_height = header_contents.style.clientHeight;
+var last_scroll_location = 0;
+var intro_video_contents_arrow = document.getElementById('intro_video_contents_arrow');
+var sub_column = document.getElementById('sub_column');
+
+document.addEventListener('scroll', function() {
+  /*
+  let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
+  let windowHeight = window.innerHeight; // 스크린 창
+  let fullHeight = document.body.scrollHeight; //  margin 값은 포함 x
+
+  console.log(scrollLocation);
+  console.log(windowHeight);
+  console.log(fullHeight);
+  if(scrollLocation + windowHeight >= fullHeight){
+    console.log('끝')
+  }
+  */
+  //document.getElementById('works_main_contents').scrollIntoView({behavior: 'smooth', block: 'center' });
+  let scrollLocation = document.documentElement.scrollTop; // 현재 스크롤바 위치
+  //console.log(scrollLocation);
+  
+  if(scrollLocation > 100 && scrollLocation < sub_header_height && !init_scroll_move){
+    var offset = $("#works_main_contents").offset();
+    $('html, body').animate({scrollTop : offset.top}, 1200);
+    console.log('go to main contents');
+    init_scroll_move = true;
+  }
+
+  if(init_scroll_move){
+    if(scrollLocation < last_scroll_location){
+      header_contents.style.display = "block";
+      console.log('header_height ' + header_height);
+      sub_column.style.top = "70px";
+    } else { 
+      header_contents.style.display = "none";
+      sub_column.style.top = 0;
+    }
+  }
+
+  if(scrollLocation > sub_header_height){
+    sub_column.style.position = "fixed";
+  } else {
+    sub_column.style.position = "static";
+  }
+
+  last_scroll_location = scrollLocation;
+});
+
+intro_video_contents_arrow.addEventListener('click', function () {
+  var offset = $("#intro_text_contents").offset();
+  $('html, body').animate({scrollTop : offset.top}, 1200);
+  console.log('go to intro_text_contents');
+});
